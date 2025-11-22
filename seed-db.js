e1b4f259e978f5d329a48d42c83f1dfaadce5560
Dmytro Yevtushenko <yevtushenkodmytrov@gmail.com>
2025-11-22 17:42:59 +0200

    fix pagination

--- seed-db.js
+++ seed-db.js
@@ -3,12 +3,21 @@ import axios, { AxiosError } from 'axios';
 import fs from 'fs';
 
 const fetchAndSeed = async () => {
+  const numberOfPages = 5;
+  let allCharacters = [];
   try {
-    console.log('Fetching data from Rick and Morty API...');
-    const { data } = await axios.get('https://rickandmortyapi.com/api/character');
+    for (let i = 0; i < numberOfPages; i++) {
+      console.log('Fetching data from Rick and Morty API...');
+      const { data } = await axios.get('https://rickandmortyapi.com/api/character', {
+        params: {
+          page: i + 1
+        }
+      })
+      allCharacters.push(...data.results);
+    }
 
     const db = {
-      characters: data.results
+      characters: allCharacters
     };
 
     fs.writeFileSync('db.json', JSON.stringify(db, null, 2));
--- src/features/characters/useCharacters.ts
+++ src/features/characters/useCharacters.ts
@@ -8,22 +8,30 @@ export const useCharacters = (page: number, name: string) => {
     const { isPending, error, data, isFetching } = useQuery<ApiResponse, AxiosError>({
         queryKey: ['characters', page, name],
         queryFn: async () => {
+            const perPage = 20;
             const response = await apiClient.get('/characters', {
                 params: {
-                    page,
+                    _page: page,
+                    _limit: perPage,
                     name: name?.length > 0 ? name : undefined
                 }
             });
 
             const rawData = response.data;
+            const totalCount = response.headers['x-total-count']
+                ? parseInt(response.headers['x-total-count'])
+                : 0;
 
+            const totalPages = Math.ceil(totalCount / perPage);
+            const hasNext = page < totalPages;
+            const hasPrev = page > 1;
             const adaptedResponse = {
                 results: Array.isArray(rawData) ? rawData : rawData.data || [],
                 info: {
-                    count: response.headers['x-total-count'] ? parseInt(response.headers['x-total-count']) : 0,
-                    pages: 0, // TODO: should be fixed
-                    next: null,
-                    prev: null
+                    count: totalCount,
+                    pages: Math.ceil(totalCount / perPage),
+                    next: hasNext ? `?page=${page + 1}` : null,
+                    prev: hasPrev ? `?page=${page - 1}` : null
                 }
             };
 
